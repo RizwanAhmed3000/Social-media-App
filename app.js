9d7232123cfcf3c6c5b5bdab65646af38bdcc4c0
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-05-27 13:31:31 +0500

    signup handler

--- app.js
+++ app.js
@@ -4,29 +4,29 @@ const newPassword = document.querySelector('#new-password')
 const dateOfBirth = document.querySelector('#dateOfBirth')
 const phoneNumber = document.querySelector('#phone-number')
 let gender;
-const users = JSON.parse(localStorage.getItem('users')) ||  []
+const users = JSON.parse(localStorage.getItem('users')) || []
 console.log(users)
 
-function signupHandler(){
-    if(firstName.value == "" || lastName.value == "" || phoneNumber.value == "" || newPassword.value == "" || dateOfBirth.value == "" || gender == undefined){
+function signupHandler() {
+    if (firstName.value == "" || lastName.value == "" || phoneNumber.value == "" || newPassword.value == "" || dateOfBirth.value == "" || gender == undefined) {
         alert("fill all the fields")
-        
-    }else{
-        if(newPassword.value.length < 7){
+
+    } else {
+        if (newPassword.value.length < 7) {
             alert("password must be atleast 8 characters long")
             return
-        }else if(firstName.value.includes(" ") || lastName.value.includes(" ")){
+        } else if (firstName.value.includes(" ") || lastName.value.includes(" ")) {
             alert("White space not all")
             return
         }
-       const usersDetails = {
-        firstName: firstName.value,
-        lastName: lastName.value,
-        Password: newPassword.value,
-        phoneNumber: phoneNumber.value,
-        dateOfBirth: new Date(`${dateOfBirth.value}`),
-        gender
-       }
+        const usersDetails = {
+            firstName: firstName.value,
+            lastName: lastName.value,
+            Password: newPassword.value,
+            phoneNumber: phoneNumber.value,
+            dateOfBirth: new Date(`${dateOfBirth.value}`),
+            gender
+        }
         // console.log(usersDetails)
         users.push(usersDetails)
         // console.log(users)
@@ -35,6 +35,6 @@ function signupHandler(){
     }
 }
 
-function genderHandler(g){
+function genderHandler(g) {
     gender = g
 }
