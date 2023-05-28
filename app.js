87cbb84f2ecf970a4014e16c052e2acc487c51b2
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-05-28 13:58:13 +0500

    showHide password

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 const firstName = document.querySelector('#first-name')
 const lastName = document.querySelector('#last-name')
 const newPassword = document.querySelector('#new-password')
+const password = document.querySelector('#password')
+// console.log(password)
 const dateOfBirth = document.querySelector('#dateOfBirth')
-const phoneNumber = document.querySelector('#phone-number')
+const emailAddress = document.querySelector('#email-address')
+const passwordWarning = document.querySelector('.passwordWarning')
+const passwordBtn = document.querySelector('.passwordBtn')
 let gender;
+// console.log(passwordBtn)
+
+
+
+
+passwordWarning.style.visibility = "hidden"
 const users = JSON.parse(localStorage.getItem('users')) || []
 console.log(users)
 
+function showHide(p){
+    if(p.type == "password"){
+        p.setAttribute('type', 'text')
+        passwordBtn.classList.add('active')
+    }else{
+        p.setAttribute('type', 'password')
+        passwordBtn.classList.remove('active')
+    }
+}
+
 function signupHandler() {
-    if (firstName.value == "" || lastName.value == "" || phoneNumber.value == "" || newPassword.value == "" || dateOfBirth.value == "" || gender == undefined) {
+    if (firstName.value == "" || lastName.value == "" || emailAddress.value == "" || newPassword.value == "" || dateOfBirth.value == "" || gender == undefined) {
         alert("fill all the fields")
 
     } else {
         if (newPassword.value.length < 7) {
-            alert("password must be atleast 8 characters long")
+            passwordWarning.style.visibility = "visible"
+            setTimeout(()=>{
+                passwordWarning.style.visibility = "hidden"
+            }, 3000)
             return
         } else if (firstName.value.includes(" ") || lastName.value.includes(" ")) {
             alert("White space not all")
@@ -23,7 +46,7 @@ function signupHandler() {
             firstName: firstName.value,
             lastName: lastName.value,
             Password: newPassword.value,
-            phoneNumber: phoneNumber.value,
+            emailAddress: emailAddress.value,
             dateOfBirth: new Date(`${dateOfBirth.value}`),
             gender
         }
@@ -32,6 +55,14 @@ function signupHandler() {
         // console.log(users)
         localStorage.setItem('users', JSON.stringify(users))
         alert("signup successfull")
+        firstName.value = ""
+        lastName.value = ""
+        emailAddress.value = ""
+        newPassword.value = ""
+        dateOfBirth.value = ""
+        dateOfBirth.value = ""
+        gender = ""
+
     }
 }
 
